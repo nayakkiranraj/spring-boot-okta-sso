4c2b2d7db599d1e56feb3ee810e8c0fa4ef71760
knaya343 <Kiranrajgourish.Nayak@marriott-sp.com>
2019-08-09 22:58:14 +0530

    sso

--- src/main/java/com/example/okta/sso/SpringBootOktaSsoGreetingController.java
+++ src/main/java/com/example/okta/sso/SpringBootOktaSsoGreetingController.java
@@ -2,11 +2,8 @@ package com.example.okta.sso;
 
 import java.security.Principal;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.oauth2.client.resource.UserRedirectRequiredException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
 
 @RestController
 public class SpringBootOktaSsoGreetingController {
@@ -15,13 +12,5 @@ public class SpringBootOktaSsoGreetingController {
 	public String greetUser(Principal principal) {
 		return "Hello User : "+principal.getName();
 	}
-	
-	@Bean
-    public SimpleMappingExceptionResolver exceptionResolver() {
-        SimpleMappingExceptionResolver exceptionResolver = new SimpleMappingExceptionResolver();
-           exceptionResolver
-                .setExcludedExceptions(UserRedirectRequiredException.class);
-        return exceptionResolver;
-    }
 	
 }
